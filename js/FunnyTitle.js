1e97c90d71e9aa78aa9a2eec9f4c809d74dbf159
zhangweiqi <zwqdddd@qq.com>
2024-01-07 16:39:53 +0800

    Site updated: 2024-01-07 16:39:51

---
+++ js/FunnyTitle.js
@@ -0,0 +1,18 @@
+
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/image/beautify/xinsuixuanzhong.png");
+        document.title = '_(:з」∠)_不要走！';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/image/beautify/favicon.ico");
+        //document.title = '(*/ω＼*)你来啦' + OriginTitle;
+        document.title = '(*/ω＼*)你来啦~~';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
